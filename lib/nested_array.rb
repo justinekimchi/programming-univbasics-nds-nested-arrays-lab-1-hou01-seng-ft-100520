1f042b13eaa1de9aac9f51032c509c535707ce80
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-12 01:42:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,12 +49,9 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[row][column]= "Onions"
+  matrix[row][column]= "new_value"
   
   matrix
-  
-  
-  
   
   
   # Given any matrix (array of arrays), a row index and a column index, 
